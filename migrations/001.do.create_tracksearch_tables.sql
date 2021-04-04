1e9f79a4b8e382c23d8aaed9a6698795dc014ecb
phongho23 <ho.phongt@gmail.com>
2021-04-04 18:49:06 -0400

    minor change

--- migrations/001.do.create_tracksearch_tables.sql
+++ migrations/001.do.create_tracksearch_tables.sql
@@ -9,51 +9,9 @@ CREATE TABLE jobs (
     "weekId" INTEGER REFERENCES weeks(id) ON DELETE CASCADE NOT NULL,
     modified TIMESTAMPTZ DEFAULT now() NOT NULL,
     content TEXT DEFAULT '', 
-    -- (job description) is content
     jobrating INTEGER,
     interview TEXT DEFAULT '',
     postedurl TEXT DEFAULT '',
     companyname TEXT DEFAULT ''
 );
 
--- // "id": "cbc787a0-ffaf-11e8-8eb2-f2801f1b9fd1",
--- // "name": "Software Engineer I - Target", 
--- // "modified": "2021-01-03T00:00:00.000Z", 
--- // "weekId": "b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1", 
--- // "jobtitle": "Software Engineer",
--- // "companyname": "Target",
--- // "postedurl": "http://www.URLholder.com/",
--- // "interview": "Scheduled for 02/20/2021",
--- // "jobrating": "5",
--- // "content": "Corporis accusamus -
-
--- INSERT INTO weeks (name)
-
--- VALUES
---   ('Week 1'),
---   ('Week 2'),
---   ('Week 3');
-
--- INSERT INTO jobs (modified, "weekId", jobtitle, companyname, postedurl, interview, jobrating, content)
-
--- VALUES
---   ('2021-01-03T00:00:00.000Z', 1,
---    'Software Engineer - Target', 'Target', 'http://www.URLholder.com/', 'Scheduled for 02/20/2021', 5, 
---    'Corporis accusamus placeat quas non voluptas. Harum fugit molestias qui. Velit ex animi reiciendis quasi. Suscipit totam delectus ut voluptas aut qui rerum. Non veniam eius molestiae rerum quam.\n \rUnde qui aperiam praesentium alias. Aut temporibus id quidem recusandae voluptatem ut eum. Consequatur asperiores et in quisquam corporis maxime dolorem soluta. Et officiis id est quia sunt qui iste reiciendis saepe. Ut aut doloribus minus non nisi vel corporis. Veritatis mollitia et molestias voluptas neque aspernatur reprehenderit.\n \rMaxime aut reprehenderit mollitia quia eos sit fugiat exercitationem. Minima dolore soluta. Quidem fuga ut sit voluptas nihil sunt aliquam dignissimos. Ex autem nemo quisquam voluptas consequuntur et necessitatibus minima velit. Consequatur quia quis tempora minima. Aut qui dolor et dignissimos ut repellat quas ad.'
---   ),
---   ('2021-08-15T23:00:00.000Z', 2,
---    'Software Engineer - Wal-mart', 'Wal-Mart', 'http://www.URLholder.com/', 'Scheduled for 02/20/2021', 5, 
---    'Eos laudantium quia ab blanditiis temporibus necessitatibus. Culpa et voluptas ut sed commodi. Est qui ducimus id. Beatae sint aspernatur error ullam quae illum sint eum. Voluptas corrupti praesentium soluta cumque illo impedit vero omnis nisi.\n \rNam sunt reprehenderit soluta quis explicabo impedit id. Repudiandae eligendi libero ad ut dolores. Laborum nihil sint et labore iusto reiciendis cum. Repellat quos recusandae natus nobis ullam autem veniam id.\n \rEsse blanditiis neque tempore ex voluptate commodi nemo. Velit sapiente at placeat eveniet ut rem. Quidem harum ut dignissimos. Omnis pariatur quas aperiam. Quasi voluptas qui nulla. Iure quas veniam aut quia et.'
---   ),
---     ('2021-03-01T00:00:00.000Z', 3,
---    'Software Engineer - Lockheed', 'Lockheed Martin', 'http://www.URLholder.com/', 'Scheduled for 02/20/2021', 5, 
---    'Occaecati dignissimos quam qui facere deserunt quia. Quaerat aut eos laudantium dolor odio officiis illum. Velit vel qui dolorem et.\n \rQui ut vel excepturi in at. Ut accusamus cumque quia sapiente ut ipsa nesciunt. Dolorum quod eligendi qui aliquid sint.\n \rAt id deserunt voluptatem et rerum. Voluptatem fuga tempora aut dignissimos est odio maiores illo. Fugiat in ad expedita voluptas voluptatum nihil.'
---   ),
---     ('2021-01-04T00:00:00.000Z', 1,
---    'Software Engineer - Google', 'Google', 'http://www.URLholder.com/', 'Scheduled for 02/20/2021', 5, 
---    'Eum culpa odit. Veniam porro molestiae dolores sunt reiciendis culpa. Atque accusamus dolore eos odio facilis. Dolores reprehenderit et provident dolores possimus mollitia.\n \rAdipisci dolor necessitatibus nihil quod quia vel veniam. Placeat qui vero. Cum cum amet at nisi. Distinctio rerum similique explicabo atque ratione. Recusandae omnis earum est. Quas iusto nihil itaque architecto ea.\n \rPerferendis neque doloremque quibusdam accusantium ut dolor illum dolorum. Vero et similique nihil beatae. In repellendus dolores praesentium. Optio alias rerum culpa placeat maiores natus sed. Ipsa et qui cum ex maiores.'
---   ),
---     ('2021-03-03T00:00:00.000Z', 2,
---    'Software Engineer - USPS', 'USPS', 'http://www.URLholder.com/', 'Scheduled for 02/20/2021', 5, 
---    'Eaque aliquid sit. Ducimus consequatur nam. Corporis ut dolorum amet molestiae minima ut quasi nesciunt. Ad omnis et. Dolorem nemo id non voluptatem mollitia sit laudantium. Consequatur quia consequuntur praesentium perferendis alias molestiae voluptatem qui temporibus.\n \rRerum tempore sed eum delectus excepturi odio. Ipsam omnis occaecati qui. Ut aspernatur et et est consequatur architecto laboriosam. Voluptates ullam beatae vero.\n \rAmet magnam neque temporibus totam aperiam aut dignissimos. Voluptas aut est ut quisquam totam. Dolor quibusdam nesciunt voluptatem cum sit harum in dicta. Sed nostrum et veniam id hic voluptatem inventore quo. Ipsam officiis unde dolor eum est dolores qui itaque aspernatur. Magnam aliquam qui qui.'
---   );  
